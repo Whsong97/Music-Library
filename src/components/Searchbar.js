d834b382a4049ae8dc486cd82db2d7060459b25c
Wing <winghangsong@gmail.com>
2023-03-12 14:17:45 -0400

    Music Library

---
+++ src/components/Searchbar.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function SearchBar(props) {
+   // let [searchTerm, setSearchTerm] = useState('')
+
+    return (
+        <form>
+            <input type="text" placeholder="Search Here" 
+            onChange={
+                (e) => props.handleSearch(e, e.target.value)
+                } />
+            <input type="submit" />
+        </form>
+    )
+}
+    
+
+export default SearchBar
